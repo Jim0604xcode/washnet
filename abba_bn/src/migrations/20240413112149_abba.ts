6b70f61675cfa5c72d9846ff791df731db70d24a
JimChan <jim.chan@JimChans-MacBook-Pro.local>
2024-04-13 19:28:08 +0800

    revised migration table

---
+++ abba_bn/src/migrations/20240413112149_abba.ts
@@ -0,0 +1,64 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    await knex.schema.createTable("users",(table)=>{
+        table.text("id").notNullable().unique().primary();
+        table.string("display_name").notNullable();
+        table.string("mobile").notNullable().unique()
+        table.string("email").notNullable().unique()
+        table.string("password").notNullable()
+        table.enu('role',["admin","customer"]).notNullable()
+        table.timestamp("created_at").defaultTo(knex.fn.now())
+        table.timestamp("updated_at")
+    })
+    await knex.schema.createTable("customer_meta",(table)=>{
+        table.string("area").notNullable()
+        table.string("street").notNullable()
+        table.string("house").notNullable()
+        table.string("room").notNullable()
+        
+        table.text("customer_id").unsigned();
+        table.foreign("customer_id").references("users.id");
+    })
+    await knex.schema.createTable("staff_meta",(table)=>{
+        table.string("work_location").notNullable()
+        
+        table.text("staff_id").unsigned();
+        table.foreign("staff_id").references("users.id");
+    })
+    await knex.schema.createTable("orders",(table)=>{
+        table.increments("id");
+        table.enu('order_type',["pw","dc","ws","lw","cs","fw"]).notNullable();
+        table.string("pickup_date_time").notNullable();
+        table.string("delivery_date_time").notNullable();
+        
+        table.string("full_address").notNullable();
+        table.string("remarks").nullable();
+        table.enu('status',["w_pickup","w_delivery","complete"]).notNullable();
+        
+        table.text("customer_id").unsigned();
+        table.foreign("customer_id").references("users.id");
+
+        table.timestamp("created_at").defaultTo(knex.fn.now())
+        table.timestamp("updated_at")
+        
+    })
+
+    await knex.schema.createTable("languague",(table)=>{
+        table.increments("id");
+        table.text("cn").notNullable();
+        table.text("eng").notNullable();
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    await knex.schema.dropTable("languague")
+    await knex.schema.dropTable("orders")
+    await knex.schema.dropTable("staff_meta")
+    await knex.schema.dropTable("customer_meta")
+    await knex.schema.dropTable("users")
+    
+}
+
